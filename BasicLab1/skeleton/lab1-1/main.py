38e823b2e54877cd8e71cc00507882950d214c69
2017-17018 <tae7130@snu.ac.kr>
2020-01-08 23:33:31 +0900

    lab02

--- BasicLab1/skeleton/lab1-1/main.py
+++ BasicLab1/skeleton/lab1-1/main.py
@@ -5,66 +5,59 @@ program = 'wordCnt.py'
 
 
 def grader(program, input_files, output_files, answer_files):
-    pid = os.fork()
-    if pid == 0:
-        # child process
-        for i in range(len(input_files)):
-            inputf = path + 'input' + str(input_files[i]) + '.txt'
-            outputf = path + 'output' + str(output_files[i]) + '.txt'
 
-            os.system('python3 '+program+' < '+inputf+' > '+outputf)
+    for i in range(len(input_files)):
+        inputf = path + 'input' + str(input_files[i]) + '.txt'
+        outputf = path + 'output' + str(output_files[i]) + '.txt'
+
+        os.system('python3 '+program+' < '+inputf+' > '+outputf)
+
+    # after all test is done
+    try:
+        rf = open('./record.txt', 'w')
+        wf = open('./wrong.txt', 'w')
+    except:
+        print('record fail')
         exit(0)
 
-    else:
-        # parent process
-        os.waitpid(pid, 0)
+    # recording the result
+    wf.write('')
+    total = 0
+    for i in range(len(output_files)):
+        outputf = path + 'output' + str(output_files[i]) + '.txt'
+        answerf = path + 'answer' + str(answer_files[i]) + '.txt'
 
-        # after all test is done
         try:
-            rf = open('./record.txt', 'w')
-            wf = open('./wrong.txt', 'w')
+            of = open(outputf)
+            af = open(answerf)
         except:
-            print('record fail')
-            exit(0)
+            print('fail')
+            exit(1)
 
-        # recording the result
-        wf.write('')
-        total = 0
-        for i in range(len(output_files)):
-            outputf = path + 'output' + str(output_files[i]) + '.txt'
-            answerf = path + 'answer' + str(answer_files[i]) + '.txt'
+        of_lines = of.readlines()
+        af_lines = af.readlines()
 
-            try:
-                of = open(outputf)
-                af = open(answerf)
-            except:
-                print('fail')
-                exit(1)
+        right = 0
+        for j in range(len(of_lines)):
+            if of_lines[j].strip() == af_lines[j].strip():
+                right += 1
+            else:
+                print('output',output_files[i],j+1)
+                wf.write('output ' + str(output_files[i]) + ' ' + str(j+1) + '\n')
 
-            of_lines = of.readlines()
-            af_lines = af.readlines()
+        if len(of_lines) == 0:
+            print('output ' + str(output_files[i]) + ' all')
+            wf.write('output ' + str(output_files[i]) + ' all\n')
 
-            right = 0
-            for j in range(len(of_lines)):
-                if of_lines[j].strip() == af_lines[j].strip():
-                    right += 1
-                else:
-                    print('output',output_files[i],j+1)
-                    wf.write('output ' + str(output_files[i]) + ' ' + str(j+1) + '\n')
+        rf.write(str(right) + '/' + str(len(af_lines)) + '\n')
+        total += right
+        of.close()
+        af.close()
 
-            if len(of_lines) == 0:
-                print('output ' + str(output_files[i]) + ' all')
-                wf.write('output ' + str(output_files[i]) + ' all\n')
-
-            rf.write(str(right) + '/' + str(len(af_lines)) + '\n')
-            total += right
-            of.close()
-            af.close()
-
-        rf.write(str(total))
-        rf.close()
-        wf.close()
-        exit(0)
+    rf.write(str(total))
+    rf.close()
+    wf.close()
+    exit(0)
 
 
 if __name__ == '__main__':
